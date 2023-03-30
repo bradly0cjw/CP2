205f4c18111ff57495cb01e6ce8c0c0256e3662e
bradly0cjw <bradly093@gmail.com>
2023-03-30 12:10:59 +0800

    homework1-3

--- Week6/homework1.c
+++ Week6/homework1.c
@@ -1,44 +1,79 @@
 //
 // Created by LINBEI on 3/29/2023.
 //
-#include <stdio.h>
 
-
+//#include <stdio.h>
 //int main(){
-//    int a=10,b=20,swap;
+//    int a=10,b=20,swap; //A
 //    int *p,*q;
 //    int sum,diff,product,quotient;
-//    p=&a;
+//    p=&a; //B
 //    q=&b;
-//    swap=*p;
+//    swap=*p; //C
 //    *p=*q;
 //    *q=swap;
-//    sum=*p+*q;
+//    sum=*p+*q; //D
 //    diff=*p-*q;
 //    product=*p*(*q);
 //    quotient=*p/(*q);
-//    printf("%d %d %d %d %d %d",*p,*q,sum,diff,product,quotient);
+//    printf("%d %d %d %d %d %d",*p,*q,sum,diff,product,quotient); //E
 //}
 
-int main() {
-    int arr[10],sum=0;
-    for (int i = 0, j = 2; i < 10; i++, j += 2) {
-        if (i % 2 == 0)
-            arr[i] = j;
-        else
-            arr[i] = -j;
+//#include <stdio.h>
+//int main() {
+//    int arr[10],sum=0; //A
+//    for (int i = 0, j = 2; i < 10; i++, j += 2) {
+//        if (i % 2 == 0)
+//            arr[i] = j;
+//        else
+//            arr[i] = -j;
+//    }
+//    int* p=&arr[0]; //B
+//    for (int i = 0;i<10;i++) { //C
+//        sum += *(p + i);
+//    }
+//    int max=arr[0],min=arr[0];
+//    for (int i = 0;i<10;i++) { //D
+//        if (*(p + i)>max){
+//            max=*(p+i);
+//        }else if(*(p + i)<min){
+//            min=*(p+i);
+//        }
+//    }
+//    printf("%d %d %d",sum,min,max); // E
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{ //A
+    int data;
+    struct Node *next;
+};
+int main(){
+    struct Node* head=NULL;
+    struct Node* second=NULL;
+    head=(struct Node*)malloc(sizeof (struct Node)); //B
+    second=(struct Node*)malloc(sizeof(struct Node));
+    head->data=1; //C
+    second->data=2;
+    head->next=second; //D
+    second->next=NULL;
+    struct Node* temp = head;
+    while (temp != NULL) { //E
+        printf("%d ", temp->data);
+        temp = temp->next;
     }
-    int* p=&arr[0];
-    for (int i = 0;i<10;i++) {
-        sum += *(p + i);
+    printf("\n");
+    temp=head; //F
+    head=head->next;
+    free(temp);
+    temp=head;
+    while(temp!=NULL){ //G
+        printf("%d ",temp->data);
+        temp=temp->next;
     }
-    int max=arr[0],min=arr[0];
-    for (int i = 0;i<10;i++) {
-        if (*(p + i)>max){
-            max=*(p+i);
-        }else if(*(p + i)<min){
-            min=*(p+i);
-        }
-    }
-    printf("%d %d %d",sum,min,max);
+    printf("\n");
+    free(head);
+    return 0;
 }
