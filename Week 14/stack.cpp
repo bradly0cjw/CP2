84b952f645e390eea9e6eaf04eb2b7fc19756216
bradly0cjw <bradly093@gmail.com>
2023-05-14 21:16:56 +0800

    update

--- Week 14/stack.cpp
+++ Week 14/stack.cpp
@@ -70,6 +70,24 @@ public:
         }
         cout << "\n";
     }
+    int operator~() {
+        return peek();
+    }
+    Stack& operator+=(Stack& s) {
+        if(s.is_empty()){
+            return *this;
+        }
+        else{
+            Stack temp(s),temp2; 
+            while(!temp.is_empty()){
+                temp2.push(temp.pop()); 
+            }
+            while(!temp2.is_empty()){
+                push(temp2.pop()); 
+            }
+        }
+        return *this;
+    }
     friend Stack& operator+=(Stack& s, const Stack& t);
 };
     Stack& operator<<(Stack&s,int data){
@@ -80,44 +98,22 @@ public:
         s.print_stack();
         return os;
     }
-    Stack& operator~(Stack&s){
-        s.peek();
-        return s;
-    }
     Stack& operator-(Stack&s){
         s.pop();
         return s;
     }
-    Stack& operator+=(Stack& s, const Stack& t) {
-    int newSize = s.top + t.top + 2;
-    int* newArr = new int[newSize];
-    int i;
-
-    for (i = 0; i <= s.top; i++) {
-        newArr[i] = s.arr[i];
-    }
-
-    for (int j = 0; j <= t.top; j++) {
-        newArr[i++] = t.arr[j];
-    }
-
-    delete[] s.arr;
-    s.arr = newArr;
-    s.top = newSize - 2;
-
-    return s;
-}
     Stack& operator!(Stack&s){
         while(!s.is_empty()){
             s.pop();
         }
-        cout<<"Stack is empty\n";
         return s;
     }
     Stack& operator<<(Stack&t,Stack&s){
         int popitem;
-        while(!s.is_empty()){
-            popitem=s.pop();
+        Stack temp(s);
+        
+        while(!temp.is_empty()){
+            popitem=temp.pop();
             t.push(popitem);
         }
         return t;
@@ -128,14 +124,15 @@ int main(){
     Stack s1,s2(m),s3(s1);  //overloading constructors
     cout<<s1<<s2<<s3;       //display the stack element
     s1<<1<<2<<3;            //<<:  push element
+    cout<<s1<<s2<<s3;       //overloading <<
     s2<<8<<9;               // overloading <<
     s3=s2+=s1<<4;           //+=: copy content to other stack
     cout<<s1<<s2<<s3;
     cout<<-s1<<-s2<<-s3;    // -:pop
     cout<<s1<<s2<<s3;
-    cout<<~s1<<~s2<<5;       // ~: peek
+    cout<<~s1<<~s2<<5<<endl;// ~: peek
     !s1;                    // empty stack
-    s1<<0<<s2;              // overloading <<
+    s1<<0<<s2;              // overloading << assume Let stack1 << stack2 append each item of stack2 from top to bottom to stack1.
     cout<<s1<<(s2<<-1)<<s3;
     return 0;
 
